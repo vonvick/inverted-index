c15f960765057013806cd01d4c505889be29596a
Victor Nwaiwu <victornwaiwu@victor.local>
2016-11-13 09:26:43 +0100

    repopulate app.min.js file and also fix style error in inverted-index.js

--- src/js/index.js
+++ src/js/index.js
@@ -1,8 +1,43 @@
+
 // import the angular package and other components
 
-import angular from 'angular';
+import angular from "angular";
 
-import InvertedIndex from './inverted-index.js';
+import InvertedIndex from "./inverted-index.js";
 
-angular.module('indexApp', [])
-  .controller('InvertedIndexController', InvertedIndex);
+angular.module("indexApp", [])
+  .controller("InvertedIndexController", ["$scope", ($scope) => {
+    const index = new InvertedIndex();
+
+    $scope.files = [];
+    $scope.searchResult = [];
+    $scope.message = "";
+    $scope.searchText = "I love you";
+
+    $scope.uploadFile = () => {
+      const file = document.forms["upload-form"]["json-file"].files[0];
+      if(file) {
+        if(!file.name.match(/\.json$/i)) {
+          $scope.message = "Invalid file format";
+          return;
+        }
+        const reader = new FileReader();
+        reader.onload = (evt) => {
+          try {
+            const jsonData = JSON.parse(evt.target.result);
+            if(!(jsonData[0].title || jsonData[0].text)) {
+              $scope.message = "The .json file did not follow " +
+                "the required format";
+              return;
+            }
+            console.log(jsonData);
+          }
+          catch(error) {
+            $scope.message = "Invalid .json file";
+          }
+        };
+        reader.readAsBinaryString(file);
+      }
+
+    };
+  }]);
