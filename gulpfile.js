3e7a875e31843b37bccda59a60d29e696fcc7ecc
Victor Nwaiwu <victor.nwaiwu@andela.com>
2016-11-19 20:05:13 +0100

    Revert "Add layout to the User's interface and integrate angular.js"

--- gulpfile.js
+++ gulpfile.js
@@ -8,98 +8,88 @@ var vinylBuffer = require("vinyl-buffer");
 var plugins = require("gulp-load-plugins")();
 
 var src = {
-  html: "./src/**/*.html",
-  css: "./src/css/style/style.css",
-  sass: "./src/css/sass/materialize.scss",
-  scripts: {
-    all: "./src/js/**/*.js",
-    app: "./src/js/index.js"
-  },
-  images: "./src/img/*"
+	html: "./src/**/*.html",
+	css: "./src/css/style/style.css",
+	sass: "./src/css/sass/materialize.scss",
+	scripts: {
+		all: "./src/js/**/*.js",
+		app: "./src/js/inverted-index.js"
+	}
 };
 
 var build = "./build/";
 var out = {
-  html: build + "*.html",
-  css: build + "css/",
-  customcss: build + "css/style",
-  scripts: {
-    file: "app.min.js",
-    folder: build + "js/"
-  },
-  images: build + "img/"
+	html: build + "*.html",
+	css: build + "css/",
+	customcss: build + "css/style",
+	scripts: {
+		file: "app.min.js",
+		folder: build + "js/"
+	}
 };
 
 gulp.task("default", function() {
 
 });
 
-gulp.task("script", function() {
-  var sources = browserify({
-    entries: src.scripts.app,
-    debug: true
-  })
-  .transform(babelify.configure({
-    presets: ["es2015"]
-  }));
-
-  return sources.bundle()
-    .pipe(vinylSourceStream(out.scripts.file))
-    .pipe(vinylBuffer())
-    .pipe(gulp.dest(out.scripts.folder))
-    .pipe(plugins.connect.reload());
-});
-
 gulp.task("sass", function() {
-  return gulp.src(src.sass)
+	return gulp.src(src.sass)
     .pipe(plugins.sass())
     .pipe(gulp.dest(out.css));
 });
 
 gulp.task("css", function() {
-  return gulp.src(src.css)
+	return gulp.src(src.css)
     .pipe(cleanCSS())
     .pipe(gulp.dest(out.customcss))
-    .pipe(plugins.connect.reload());
+		.pipe(plugins.connect.reload());
+});
+
+gulp.task("script", function() {
+	var sources = browserify({
+		entries: src.scripts.app,
+		debug: true
+  })
+  .transform(babelify.configure({
+		presets: ["es2015"]
+	}));
+
+  return sources.bundle()
+    .pipe(vinylSourceStream(out.scripts.file))
+    .pipe(vinylBuffer())
+    .pipe(gulp.dest(out.scripts.folder));
 });
 
 gulp.task("html", function() {
-  return gulp.src(src.html)
-    .pipe(gulp.dest(build))
-    .pipe(plugins.connect.reload());
-});
-
-gulp.task("imagemin", function() {
-  return gulp.src(src.images)
-    .pipe(plugins.imagemin())
-    .pipe(gulp.dest(out.images));
+	return gulp.src(src.html)
+	.pipe(gulp.dest(build))
+	.pipe(plugins.connect.reload());
 });
 
 /* The jshint task runs jshint with ES6 support. */
 gulp.task("jshint", function() {
-  return gulp.src(src.scripts.all)
-    .pipe(plugins.jshint({
-      esnext: true // Enable ES6 support
-    }))
-    .pipe(plugins.jshint.reporter("jshint-stylish"));
+	return gulp.src(src.scripts.all)
+		.pipe(plugins.jshint({
+			esnext: true // Enable ES6 support
+		}))
+		.pipe(plugins.jshint.reporter("jshint-stylish"));
 });
 
 gulp.task("serve", ["build", "watch"], function() {
-  plugins.connect.server({
-    root: build,
-    port: 8080,
-    livereload: true,
-    fallback: build + "index.html"
-  });
+	plugins.connect.server({
+		root: build,
+		port: 8080,
+		livereload: true,
+		fallback: build + "index.html"
+	});
 });
 
 gulp.task("watch", function() {
-  gulp.watch(src.sass, ["sass"]);
-  gulp.watch(src.css, ["css"]);
-  gulp.watch(src.html, ["html"]);
-  gulp.watch(src.scripts.all, ["script"]);
-  gulp.watch(src.images, ["imagemin"]);
+	gulp.watch(src.sass, ["sass"]);
+	gulp.watch(src.css, ["css"]);
+	gulp.watch(src.html, ["html"]);
+	gulp.watch(src.scripts.all, ["script"]);
 });
 
-gulp.task("build", ["script", "sass", "css", "html", "imagemin"]);
+gulp.task("build", ["script", "sass", "css", "html"]);
 gulp.task("default", ["serve"]);
