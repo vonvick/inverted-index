7d40bf8f824ec6c8a1d75ddd97097969c7f4f9a1
Victor Nwaiwu <victor.nwaiwu@andela.com>
2016-11-19 20:07:01 +0100

    Revert "Angular integration - integrate angular and inverted-index class into the entry point of the application."

--- src/js/index.js
+++
@@ -1,8 +0,0 @@
-// import the angular package and other components
-
-import angular from 'angular';
-
-import InvertedIndex from './inverted-index.js';
-
-angular.module('indexApp', [])
-  .controller('InvertedIndexController', InvertedIndex);
