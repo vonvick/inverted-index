004f27be211153c7b6b9631e8aac2eee0a386e5e
andela-vnwaiwu <victornwaiwu@victor.local>
2016-11-20 08:12:30 +0100

    coveralls: edit travis file and karma configuration to include coveralls coverage.

--- karma.conf.js
+++ karma.conf.js
@@ -28,22 +28,20 @@ module.exports = function(config) {
     // preprocess matching files before serving them to the browser
     // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
     preprocessors: {
-      "!(*spec).js": "coverage"
+      "build/js/inverted-index.js": ["coverage"]
     },
 
 
     // test results reporter to use
     // possible values: 'dots', 'progress'
     // available reporters: https://npmjs.org/browse/keyword/karma-reporter
-    reporters: ["coverage", "coveralls"],
-
-    plugins: ["karma-coverage"],
+    reporters: ["progress", "coverage"],
 
     coverageReporter: {
       type: "lcov",
-      dir: "coverage",
-      subdir: "."// Output the results into ./coverage/
+      dir: "coverage/"
     },
+
     // web server port
     port: 9876,
 
