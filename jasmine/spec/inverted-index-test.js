94e127a666c9d8d32b3abe9d89649cf42225403a
Victor Nwaiwu <victornwaiwu@victor.local>
2016-12-09 22:21:29 +0100

    test: rename variables and method names for test

--- jasmine/spec/inverted-index-test.js
+++ jasmine/spec/inverted-index-test.js
@@ -16,28 +16,32 @@ describe('Inverted Index', () => {
     invertedIndex.indexes = {};
   });
 
-  it('should return the instance of the class', () => {
+  it('should be truthy for the instance of the class', () => {
     const indexInstance = invertedIndex;
-    expect(indexInstance).toEqual(jasmine.any(Object));
+    expect(invertedIndex instanceof InvertedIndex).toBeTruthy();
+  });
+
+  it('should return zero for the length of indexes', () => {
+    const indexInstance = invertedIndex;
     expect(Object.keys(invertedIndex.indexes).length).toBe(0);
   });
 
   describe('Read book data', () => {
     it('should read the content of a json file and return false if the file is empty', () => {
       const book = [];
-      const readBook = invertedIndex.checkBookData(book);
-      expect(readBook).toBe(false);
+      const isValid = invertedIndex.readFileData(book);
+      expect(isValid).toBe(false);
     });
     it('should read the content of a json file and return true if the file matches the format', () => {
-      const readBook2 = invertedIndex.checkBookData(books);
-      expect(readBook2).toBe(true);
+      const isValid = invertedIndex.readFileData(books);
+      expect(isValid).toBe(true);
     });
     it('should return false if the content of the file is not an array of object', () => {
-      const readBook2 = invertedIndex.checkBookData(badFile);
-      expect(readBook2).toBe(false);
+      const isValid = invertedIndex.readFileData(badFile);
+      expect(isValid).toBe(false);
     });
     it('should return false if the content of the file is a multi-dimensional array', () => {
-      const wrongFormat = invertedIndex.checkBookData(deepFile);
+      const wrongFormat = invertedIndex.readFileData(deepFile);
       expect(wrongFormat).toBe(false);
     });
   });
@@ -97,19 +101,19 @@ describe('Inverted Index', () => {
   describe('Search Index', () => {
     it('should return an array containing the index of a word', () => {
       invertedIndex.createIndex('books', books);
-      const searchIndex = invertedIndex.searchIndex('and', 'books');
+      const searchIndex = invertedIndex.searchIndex('books', 'and');
       expect(searchIndex[0].books.and).toEqual([0, 1]);
     });
     it('should return null for a word not found in the file', () => {
       invertedIndex.createIndex('books', books);
-      const searchIndex = invertedIndex.searchIndex('because', 'books');
+      const searchIndex = invertedIndex.searchIndex('books', 'because');
       expect(searchIndex[0].books.because).toBe(null);
     });
     it('should return an array of Objects with keys of all the files searched', () => {
       invertedIndex.createIndex('books', books);
       invertedIndex.createIndex('books1', books1);
       const fileList = ['books', 'books1'];
-      const searchIndex = invertedIndex.searchIndex('and', 'all');
+      const searchIndex = invertedIndex.searchIndex('all', 'and');
       searchIndex.forEach((file) => {
         expect(fileList).toContain(Object.keys(file)[0]);
       });
@@ -118,15 +122,15 @@ describe('Inverted Index', () => {
     it('should return null if a word is not found in a file', () => {
       invertedIndex.createIndex('books', books);
       invertedIndex.createIndex('books1', books1);
-      const searchIndex = invertedIndex.searchIndex('because', 'all');
+      const searchIndex = invertedIndex.searchIndex('all', 'because');
       expect(searchIndex[0].books.because).toBe(null);
       expect(searchIndex[1].books1.because).toBe(null);
     });
     it('should return an array containing the indexes of the search parameter in the selected file', () => {
       invertedIndex.createIndex('books', books);
       invertedIndex.createIndex('books1', books1);
-      const searchIndex = invertedIndex.searchIndex('and', 'books');
-      const searchIndex2 = invertedIndex.searchIndex('and', 'books1');
+      const searchIndex = invertedIndex.searchIndex('books', 'and');
+      const searchIndex2 = invertedIndex.searchIndex('books1', 'and');
       expect(searchIndex[0].books.and).toEqual([0, 1]);
       expect(Object.keys(searchIndex[0]).length).toBe(1);
       expect(searchIndex2[0].books1.and).toEqual([0, 1]);
